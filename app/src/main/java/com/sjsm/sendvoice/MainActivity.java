06b87c12cf4da1ca270ded5de2061310f64b3425
sjsm <wonner_cw@163.com>
2017-08-22 11:56:25 +0800

    Initial commit

---
+++ app/src/main/java/com/sjsm/sendvoice/MainActivity.java
@@ -0,0 +1,361 @@
+package com.sjsm.sendvoice;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int VOICE_REQUEST_CODE = 66;
+
+    private Button mButton,mBtnSend;
+    private ImageView volume,mSendVoice,img1,sc_img1;
+    private RelativeLayout mBottom;
+    private TextView mTextView,mBtnRcd;
+    private AudioRecoderUtils mAudioRecoderUtils;
+    private Context context;
+    private PopupWindowFactory mPop;
+    private RelativeLayout rl;
+    private LinearLayout del_re;
+    private LinearLayout voice_rcd_hint_loading, voice_rcd_hint_rcding,
+            voice_rcd_hint_tooshort;
+    private boolean btn_vocie = false;
+    private boolean isShosrt = false;
+    private int flag = 1;
+    private View rcChat_popup;
+    private long startVoiceT, endVoiceT;
+    private Handler mHandler = new Handler();
+    private String voiceName;
+    private ChatMsgViewAdapter mAdapter;
+    private List<ChatMsgEntity> mDataArrays = new ArrayList<ChatMsgEntity>();
+    private ListView mListView;
+    private SoundMeter mSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+
+        initWidget();
+        init();
+        requestPermissions();//6.0以上权限获取
+    }
+
+    /**
+     * 开启扫描之前判断权限是否打开
+     */
+    private void requestPermissions() {
+        //判断是否开启语音权限
+        if ((ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(context,
+                        Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)
+                ) {
+            mSensor.start();
+
+        } else {
+            //请求开启语音权限
+            ActivityCompat.requestPermissions((Activity) context,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, VOICE_REQUEST_CODE);
+        }
+
+    }
+
+    /**
+     * 请求权限回调
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == VOICE_REQUEST_CODE) {
+            if ((grantResults[0] == PackageManager.PERMISSION_GRANTED) && (grantResults[1] == PackageManager.PERMISSION_GRANTED) ) {
+                mSensor.start();
+            } else {
+                Toast.makeText(context, "已拒绝权限！", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void initWidget() {
+        mListView = (ListView) findViewById(R.id.listview);
+        rl = (RelativeLayout) findViewById(R.id.rl);
+        mSendVoice= (ImageView) findViewById(R.id.ivPopUp);
+        mBottom = (RelativeLayout) findViewById(R.id.btn_bottom);
+        mBtnRcd = (TextView) findViewById(R.id.btn_rcd);
+        del_re = (LinearLayout) this.findViewById(R.id.del_re);
+        rcChat_popup = this.findViewById(R.id.rcChat_popup);
+        img1 = (ImageView) this.findViewById(R.id.img1);
+        sc_img1 = (ImageView) this.findViewById(R.id.sc_img1);
+        volume = (ImageView) this.findViewById(R.id.volume);
+        voice_rcd_hint_rcding = (LinearLayout) this
+                .findViewById(R.id.voice_rcd_hint_rcding);
+        voice_rcd_hint_loading = (LinearLayout) this
+                .findViewById(R.id.voice_rcd_hint_loading);
+        voice_rcd_hint_tooshort = (LinearLayout) this
+                .findViewById(R.id.voice_rcd_hint_tooshort);
+        mSensor = new SoundMeter();
+    }
+
+    private void init() {
+        mSendVoice.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                if (btn_vocie) {
+                    mBtnRcd.setVisibility(View.GONE);
+                    mBottom.setVisibility(View.VISIBLE);
+                    btn_vocie = false;
+                    mSendVoice
+                            .setImageResource(R.drawable.chatting_setmode_msg_btn);
+
+                } else {
+                    mBtnRcd.setVisibility(View.VISIBLE);
+                    mBottom.setVisibility(View.GONE);
+                    mSendVoice
+                            .setImageResource(R.drawable.chatting_setmode_voice_btn);
+                    btn_vocie = true;
+                }
+            }
+        });
+
+        mBtnRcd.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+
+        mAdapter = new ChatMsgViewAdapter(this, mDataArrays);
+        mListView.setAdapter(mAdapter);
+    }
+
+    private String getDate() {
+        Calendar c = Calendar.getInstance();
+
+        String year = String.valueOf(c.get(Calendar.YEAR));
+        String month = String.valueOf(c.get(Calendar.MONTH));
+        String day = String.valueOf(c.get(Calendar.DAY_OF_MONTH) + 1);
+        String hour = String.valueOf(c.get(Calendar.HOUR_OF_DAY));
+        String mins = String.valueOf(c.get(Calendar.MINUTE));
+
+        StringBuffer sbBuffer = new StringBuffer();
+        sbBuffer.append(year + "-" + month + "-" + day + " " + hour + ":"
+                + mins);
+
+        return sbBuffer.toString();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (!Environment.getExternalStorageDirectory().exists()) {
+            Toast.makeText(this, "No SDCard", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (btn_vocie) {
+            System.out.println("1");
+            int[] location = new int[2];
+            mBtnRcd.getLocationInWindow(location);
+            int btn_rc_Y = location[1];
+            int btn_rc_X = location[0];
+            int[] del_location = new int[2];
+            del_re.getLocationInWindow(del_location);
+            int del_Y = del_location[1];
+            int del_x = del_location[0];
+            if (event.getAction() == MotionEvent.ACTION_DOWN && flag == 1) {
+                if (!Environment.getExternalStorageDirectory().exists()) {
+                    Toast.makeText(this, "No SDCard", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                System.out.println("2");
+                if (event.getY() > btn_rc_Y && event.getX() > btn_rc_X) {
+                    System.out.println("3");
+                    mBtnRcd.setBackgroundResource(R.drawable.voice_rcd_btn_pressed);
+                    rcChat_popup.setVisibility(View.VISIBLE);
+                    voice_rcd_hint_loading.setVisibility(View.VISIBLE);
+                    voice_rcd_hint_rcding.setVisibility(View.GONE);
+                    voice_rcd_hint_tooshort.setVisibility(View.GONE);
+                    mHandler.postDelayed(new Runnable() {
+                        public void run() {
+                            if (!isShosrt) {
+                                voice_rcd_hint_loading.setVisibility(View.GONE);
+                                voice_rcd_hint_rcding
+                                        .setVisibility(View.VISIBLE);
+                            }
+                        }
+                    }, 300);
+                    img1.setVisibility(View.VISIBLE);
+                    del_re.setVisibility(View.GONE);
+                    startVoiceT = System.currentTimeMillis();
+                    voiceName = startVoiceT + ".amr";
+                    start(voiceName);
+                    flag = 2;
+                }
+            } else if (event.getAction() == MotionEvent.ACTION_UP && flag == 2) {
+                System.out.println("4");
+                mBtnRcd.setBackgroundResource(R.drawable.voice_rcd_btn_nor);
+                if (event.getY() >= del_Y
+                        && event.getY() <= del_Y + del_re.getHeight()
+                        && event.getX() >= del_x
+                        && event.getX() <= del_x + del_re.getWidth()) {
+                    rcChat_popup.setVisibility(View.GONE);
+                    img1.setVisibility(View.VISIBLE);
+                    del_re.setVisibility(View.GONE);
+                    stop();
+                    flag = 1;
+                    File file = new File(android.os.Environment.getExternalStorageDirectory()+"/"
+                            + voiceName);
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                } else {
+
+                    voice_rcd_hint_rcding.setVisibility(View.GONE);
+                    stop();
+                    endVoiceT = System.currentTimeMillis();
+                    flag = 1;
+                    int time = (int) ((endVoiceT - startVoiceT) / 1000);
+                    if (time < 1) {
+                        isShosrt = true;
+                        voice_rcd_hint_loading.setVisibility(View.GONE);
+                        voice_rcd_hint_rcding.setVisibility(View.GONE);
+                        voice_rcd_hint_tooshort.setVisibility(View.VISIBLE);
+                        mHandler.postDelayed(new Runnable() {
+                            public void run() {
+                                voice_rcd_hint_tooshort
+                                        .setVisibility(View.GONE);
+                                rcChat_popup.setVisibility(View.GONE);
+                                isShosrt = false;
+                            }
+                        }, 500);
+                        return false;
+                    }
+                    ChatMsgEntity entity = new ChatMsgEntity();
+                    entity.setDate(getDate());
+                    entity.setName("高富帅");
+                    entity.setMsgType(false);
+                    entity.setTime(time+"\"");
+                    entity.setText(voiceName);
+                    mDataArrays.add(entity);
+                    mAdapter.notifyDataSetChanged();
+                    mListView.setSelection(mListView.getCount() - 1);
+                    rcChat_popup.setVisibility(View.GONE);
+
+                }
+            }
+            if (event.getY() < btn_rc_Y) {
+                System.out.println("5");
+                Animation mLitteAnimation = AnimationUtils.loadAnimation(this,
+                        R.anim.cancel_rc);
+                Animation mBigAnimation = AnimationUtils.loadAnimation(this,
+                        R.anim.cancel_rc2);
+                img1.setVisibility(View.GONE);
+                del_re.setVisibility(View.VISIBLE);
+                del_re.setBackgroundResource(R.drawable.voice_rcd_cancel_bg);
+                if (event.getY() >= del_Y
+                        && event.getY() <= del_Y + del_re.getHeight()
+                        && event.getX() >= del_x
+                        && event.getX() <= del_x + del_re.getWidth()) {
+                    del_re.setBackgroundResource(R.drawable.voice_rcd_cancel_bg_focused);
+                    sc_img1.startAnimation(mLitteAnimation);
+                    sc_img1.startAnimation(mBigAnimation);
+                }
+            } else {
+
+                img1.setVisibility(View.VISIBLE);
+                del_re.setVisibility(View.GONE);
+                del_re.setBackgroundResource(0);
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private static final int POLL_INTERVAL = 300;
+
+    private Runnable mSleepTask = new Runnable() {
+        public void run() {
+            stop();
+        }
+    };
+    private Runnable mPollTask = new Runnable() {
+        public void run() {
+            double amp = mSensor.getAmplitude();
+            updateDisplay(amp);
+            mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+
+        }
+    };
+
+    private void start(String name) {
+        mSensor.start(name);
+        mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+    }
+
+    private void stop() {
+        mHandler.removeCallbacks(mSleepTask);
+        mHandler.removeCallbacks(mPollTask);
+        mSensor.stop();
+        volume.setImageResource(R.drawable.amp1);
+    }
+
+    private void updateDisplay(double signalEMA) {
+
+        switch ((int) signalEMA) {
+            case 0:
+            case 1:
+                volume.setImageResource(R.drawable.amp1);
+                break;
+            case 2:
+            case 3:
+                volume.setImageResource(R.drawable.amp2);
+
+                break;
+            case 4:
+            case 5:
+                volume.setImageResource(R.drawable.amp3);
+                break;
+            case 6:
+            case 7:
+                volume.setImageResource(R.drawable.amp4);
+                break;
+            case 8:
+            case 9:
+                volume.setImageResource(R.drawable.amp5);
+                break;
+            case 10:
+            case 11:
+                volume.setImageResource(R.drawable.amp6);
+                break;
+            default:
+                volume.setImageResource(R.drawable.amp7);
+                break;
+        }
+    }
+}
